8e2e99dc9b91aedd93b8a580de8da8b5d83fb705
ntachukwu <chuksmcdennis@yahoo.com>
2024-09-05 16:32:54 +0100

    add scroll to browser window

---
+++ browser.py
@@ -0,0 +1,58 @@
+from tkinter import *
+from handle_request import URL, lex
+
+WIDTH, HEIGHT = 800, 600
+HSTEP, VSTEP = 13, 18 # Horizonal Step, Vertical Step for text display
+SCROLL_STEP = 100
+
+def layout(text: str) -> list[str]:
+    display_list = []
+    cursor_x, cursor_y = HSTEP, VSTEP
+    for c in text:
+        display_list.append((cursor_x, cursor_y, c))
+        cursor_x += HSTEP
+        if cursor_x >= WIDTH - HSTEP:
+            cursor_y += VSTEP
+            cursor_x = HSTEP
+    return display_list
+        
+class Browser:
+    def __init__(self):
+        self.window = Tk()
+        self.window.bind("<Down>", self.scroll_down)
+        self.window.bind("<Up>", self.scroll_up)
+        self.scroll = 0
+        self.canvas = Canvas(
+            self.window,
+            width=WIDTH,
+            height=HEIGHT  
+            )
+        self.canvas.pack()
+
+    def scroll_down(self, e):
+        self.scroll += SCROLL_STEP
+        self.draw()
+    
+    def scroll_up(self, e):
+        self.scroll -= SCROLL_STEP
+        if self.scroll < 0:
+            self.scroll = 0
+        self.draw()
+
+    def draw(self):
+        self.canvas.delete("all")
+        for x, y, c in self.display_list:
+            self.canvas.create_text(x, y-self.scroll, text=c)
+
+    def load(self, url):
+        body = url.request()
+        text = lex(body)
+        self.display_list = layout(text)
+        self.draw()
+
+
+if __name__ == "__main__":
+    import sys
+    from handle_request import URL
+    Browser().load(URL(sys.argv[1]))
+    mainloop()
