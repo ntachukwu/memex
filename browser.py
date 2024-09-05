d098216c638dd6d113c0dd88cc13fd14a50eadc2
ntachukwu <chuksmcdennis@yahoo.com>
2024-09-05 19:58:00 +0100

    make layout resizeable

--- browser.py
+++ browser.py
@@ -5,13 +5,16 @@ WIDTH, HEIGHT = 800, 600
 HSTEP, VSTEP = 13, 18 # Horizonal Step, Vertical Step for text display
 SCROLL_STEP = 100
 
-def layout(text: str) -> list[str]:
+def layout(text: str, width, height) -> list[str]:
     display_list = []
     cursor_x, cursor_y = HSTEP, VSTEP
     for c in text:
+        if c == "\n":
+            cursor_x = HSTEP
+            cursor_y += VSTEP+5
         display_list.append((cursor_x, cursor_y, c))
         cursor_x += HSTEP
-        if cursor_x >= WIDTH - HSTEP:
+        if cursor_x >= width - HSTEP:
             cursor_y += VSTEP
             cursor_x = HSTEP
     return display_list
@@ -21,14 +24,26 @@ class Browser:
         self.window = Tk()
         self.window.bind("<Down>", self.scroll_down)
         self.window.bind("<Up>", self.scroll_up)
+        self.window.bind("<MouseWheel>", self.scroll_wheel)
+        self.window.bind("<Configure>", self.reshape_window)
         self.scroll = 0
         self.canvas = Canvas(
             self.window,
             width=WIDTH,
             height=HEIGHT  
             )
-        self.canvas.pack()
-
+        self.canvas.pack(fill=BOTH, expand=1)
+    
+    def reshape_window(self, e):
+        width, height = e.width, e.height
+        self.display_list = layout(self.text, width, height)
+        self.draw()
+        
+    def scroll_wheel(self, e):
+        if e.delta == 1:
+            return self.scroll_up(e)
+        return self.scroll_down(e)
+    
     def scroll_down(self, e):
         self.scroll += SCROLL_STEP
         self.draw()
@@ -51,8 +66,8 @@ class Browser:
 
     def load(self, url):
         body = url.request()
-        text = lex(body)
-        self.display_list = layout(text)
+        self.text = lex(body)
+        self.display_list = layout(self.text, WIDTH, HEIGHT)
         self.draw()
 
 
